5611d8fc7aace5d4d1a42564d8bbe33b55f2ea46
George Georges <p.c.g.f.t.b@gmail.com>
2024-06-02 05:58:42 +0300

    level 1 done, for the most part
    Added new pre-set Layer as UI+ for wardrobe closed image (probably pointless)
    Added functions destroyImage destroyObject, selectObject, setSelectedItem, events, InpsecItemtEvents, inpsectEnvironmentEvents to ClickManager
    Added functions changeSortingLayer to itemData
    Added functions getInspectMode to InputManager
    added images wardrobeClosed, wardrobeOpen, drawerClosed, drawerOpen, matchboxInventoryImage, noteImage, noteInventoryImage, redFrameInventoryImage, blueFrameInventoryImage, yellowFrameInventoryImage, redFramePuzzleImage, blueFramePuzzleImage, yellowFramePuzzleImage
    added objects noteInventoryPosition, matchboxInventoryPosition, redFrameInventoryPosition, blueFrameInventoryPosition, yellowFrameInventoryPosition
    added various amounts of events to each image
    changed object name "antique items sheet 1.2_6" to wardrobeClosed
    added object "antique items sheet 1.2_7" beneath wardrobe
    changed object name "antique items sheet 1.2_7" to wardrobeOpen
    changed object name "antique items sheet 1.2_0" to drawerClosed
    added object "antique items sheet 1.2_2" beneath wardrobe
    changed object name "antique items sheet 1.2_2" to drawerOpen
    added image inventoryTest for testing purposes(click on tie in inventory and then on corpse)
    added object matchbox beneath drawerClosed and attached it to the inventory system
    added an Instance for ClickManager that persists throughout gametime
    added an inspectionMode in InputManager which actives with "I" and automatically deselects any currently selected item
    created and added a note png to assets
    added object note beneath corpse and attached it to the inventory system
    split the Frames sprite into individual frame sprites (although it wouldnt let me use the slice tools so i had to get creative, may need redoing)
    added object redFrame, blueFrame, yellowFrame beneath drawerClosed and attached them to the inventory system
    added new C# script imageData in assets
    added the frame puzzle, (without the message however, as i dont know what it is)
    
    edited candle.png to create an unlit candle sprite and added it to assets
    isolated the upper right part of candle.png to get an individual lit candle sprite and added it to assets
    added object candleLit outside of scene
    added object candleUnlit above drawerClosed
    added image candleUnlitImage, candleLitImage, noteWrittenInventoryImage, safeClosed, safeNoteInventoryImage
    removed object candle
    removed lingering images after frames where placed
    added code to remove items from collectedItems list once they leave the inventory
    added interaction between candleUnlit and matchbox
    added interaction between candleLit and note
    added noteWritten to assets which is an edit to note
    added object noteWritten behind candle
    added object noteWrittenInventoryPosition
    isolated the right part of safe_Animation and added it to assets as safeOpen.png
    added object safeOpen outside of the scene
    added object safeNote beneath safeClosed
    added interaction between safeClosed and writtenNote
    added safeNote to inventory when safe opens, which when inspected gives the... something
    1a928c

--- Assets/ClickManager.cs
+++ Assets/ClickManager.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickManager : MonoBehaviour
 {
-    public Transform player;
+    private bool redFlag = false, blueFlag = false, yellowFlag = false;
+    public itemData selectedItem = null, noteWritten, safeNote;
+    public Transform player, candleLit, safeOpen;
     static float moveSpeed = 6f, itemMoveSpeed = 60f;
     GameManager gameManager;
     public Transform DefInventory;
     public void Start()  {
         gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public static ClickManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        // Ensure only one instance of ClickManager exists
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject); // If another instance already exists, destroy this one
+        }
     }
 
     public void goToItem(itemData item)  {
@@ -23,8 +42,187 @@ public class ClickManager : MonoBehaviour
         }
     }
 
-    private void attemptItemAquisition(itemData item)  {
-        if (item.requiredItemID == -1 || GameManager.CollectedItems.Contains(item))  {
+    public void selectItem(itemData item)  {
+        if (InputManager.Instance.getInspectMode())  {
+            return;
+        }
+        if (selectedItem != null && selectedItem == item)  {
+            selectedItem = null;
+            return;
+        }
+        if (GameManager.CollectedItems.Contains(item))  {
+            Debug.Log("ITEM SELECTED: " + item.itemID);
+            selectedItem = item;
+        }
+    }
+
+        public void destroyImage(Image imageToDestroy)  {
+        if (InputManager.Instance.getInspectMode())  {
+            return;
+        }
+        Destroy(imageToDestroy);
+    }
+
+    public void destroyObject(GameObject objectToDestroy)  {
+        if (InputManager.Instance.getInspectMode())  {
+            return;
+        }
+        Destroy(objectToDestroy);
+    }
+
+    public void events(imageData imageCheck)  {
+
+        if (selectedItem != null)  {
+            switch (selectedItem.itemID)  {
+                case 1:
+                    selectedItem.item.position = new Vector3(0.79f, -2.98f, 0);
+                    Debug.Log("OH THE IRONY");
+                    break;
+                case 3:
+                    if (selectedItem.itemID == imageCheck.imageID)  {
+                        candleLit.position = new Vector3(-5.73952f, 2.24513f, 0);
+                        Destroy(imageCheck.image);
+                        selectedItem = null;
+                        Debug.Log("Arson achieved");
+                    }
+                    break;
+                case 4:
+                    if (selectedItem.itemID == imageCheck.imageID)  {
+                        GameManager.CollectedItems.Remove(selectedItem);
+                        Destroy(selectedItem.inventoryImage);
+                        Destroy(selectedItem.gameObject);
+                        attemptItemAquisition(noteWritten);
+                        selectedItem = null;
+                        Debug.Log("Arson achieved");
+                    }
+                    break;
+                case 5:
+                    if (selectedItem.itemID == imageCheck.imageID)  {
+                        selectedItem.item.position = new Vector3(4.619f, 3.33f, 0);
+                        redFlag = true;
+                        GameManager.CollectedItems.Remove(selectedItem);
+                        Destroy(selectedItem.inventoryImage);
+                        selectedItem = null;
+                        Debug.Log("RED frame PLACED");
+                        if (redFlag && blueFlag && yellowFlag)  {
+                            Debug.Log("EURIKA");
+                        }
+                    }
+                    break;
+                case 6:
+                    if (selectedItem.itemID == imageCheck.imageID)  {
+                        selectedItem.item.position = new Vector3(3.62f, 3.33f, 0);
+                        Destroy(imageCheck);
+                        blueFlag = true;
+                        GameManager.CollectedItems.Remove(selectedItem);
+                        Destroy(selectedItem.inventoryImage);
+                        selectedItem = null;
+                        Debug.Log("BLUE frame PLACED");
+                        if (redFlag && blueFlag && yellowFlag)  {
+                            Debug.Log("EURIKA");
+                        }
+                    }
+                    break;
+                case 7:
+                    if (selectedItem.itemID == imageCheck.imageID)  {
+                        selectedItem.item.position = new Vector3(5.623f, 3.33f, 0);
+                        Destroy(imageCheck);
+                        yellowFlag = true;
+                        GameManager.CollectedItems.Remove(selectedItem);
+                        Destroy(selectedItem.inventoryImage);
+                        selectedItem = null;
+                        Debug.Log("YELLOW frame PLACED");
+                        if (redFlag && blueFlag && yellowFlag)  {
+                            Debug.Log("EURIKA");
+                        }
+                    }
+                    break;
+                case 8:
+                    if (selectedItem.itemID == imageCheck.imageID)  {
+                        safeOpen.position = new Vector3(5.6f, -3.3f, 0);
+                        Destroy(imageCheck.image);
+                        attemptItemAquisition(safeNote);
+                        selectedItem = null;
+                    }
+                    break;
+                default:
+                    Debug.Log("Unknown item selected.");
+                    break;
+            }
+        }
+    }
+
+    public void inpsectItemEvents(itemData item)  {
+        if (!GameManager.CollectedItems.Contains(item))  {
+            return;
+        }
+        if (InputManager.Instance.getInspectMode())  {
+            switch (item.itemID)  {
+                case 1:
+                        Debug.Log("My what a lovely tie. I just hope no-one had to die to this tie. That would just make me cry");
+                        break;
+                case 3:
+                        Debug.Log("Matchtastic");
+                        break;
+                case 4:
+                        Debug.Log("This is a piece of paper made of wood");
+                        break;
+                case 5:
+                        Debug.Log("Ze red frame");
+                        break;
+                case 6:
+                        Debug.Log("Ze blue frame");
+                        break;
+                case 7:
+                        Debug.Log("Ze yellow frame");
+                        break;
+                case 8:
+                        Debug.Log("Useless piece of paper, just has a bunch of random numbers written on it. Useless");
+                        break;
+                case 9:
+                        Debug.Log("Something something killer");
+                        break;
+                default:
+                    Debug.Log("No comment");
+                    break;
+            }
+        }
+    }
+
+    public void inpsectEnvironmentEvents(int envCode)  {
+        if (InputManager.Instance.getInspectMode())  {
+            switch (envCode)  {
+                case 0:
+                        Debug.Log("Yeeeeeeeep... he dead");
+                        break;
+                case 1:
+                        Debug.Log("It's a wardrobe");
+                        break;
+                case 2:
+                        Debug.Log("An inconspicuous UNLIT candle, hmmmmmm. What could i possibly do with an UNLIT candle");
+                        break;
+                case 3:
+                        Debug.Log("Such a nice STRONG flame, why you could probably see through objects made from wood composites with it");
+                        break;
+                case 4:
+                        Debug.Log("Nothing to see here. Just your typical floor safe");
+                        break;
+                default:
+                    Debug.Log("No comment");
+                    break;
+            }
+        }
+    }
+
+    public void setSelectedItem(itemData item)  {
+        selectedItem = item;
+    }
+
+    public void attemptItemAquisition(itemData item)  {
+        if (InputManager.Instance.getInspectMode())  {
+            return;
+        }
+        if (item.itemID > 0 && (item.requiredItemID == -1 || GameManager.CollectedItems.Contains(item)))  {
             GameManager.CollectedItems.Add(item);
             Debug.Log("itemID " + item.itemID + " has been aquired!");
             foreach (itemData collectedItem in GameManager.CollectedItems)  {
@@ -32,9 +230,11 @@ public class ClickManager : MonoBehaviour
             }
             if (InputManager.Instance.getInventoryActive() == false)  {
                 item.item.position += new Vector3(0, 2, 0); 
-                InputManager.Instance.InventoryToggle();
+                InputManager.Instance.inventoryToggle();
             }
             StartCoroutine(gameManager.MoveToPoint(item.item, item.inventoryPosition.position, item.speed));
+            item.changeSortingLayer("UI");
+            Destroy(item.itemImage);
             //item.item.position = DefInventory.position;
             //goToInventory(item);
         }
